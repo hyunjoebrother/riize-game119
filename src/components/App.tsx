28c7c3e6a4f53aa9376a37dd0cd858cab4839353
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-03-14 14:43:08 +0900

    feat: add Privacy Page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ import "./Translation/i18n";
 const Main = lazy(() => import("../pages/Main/Main"));
 const RiizeQuiz = lazy(() => import("../pages/RiizeQuiz/RiizeQuiz"));
 const HandGame = lazy(() => import("../pages/HandGame/HandGame"));
+const Privacy = lazy(() => import("../pages/Privacy/Privacy"));
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Route path="/" element={<Main />} />
         <Route path="/riize" element={<RiizeQuiz />} />
         <Route path="/handgame" element={<HandGame />} />
+        <Route path="/privacy" element={<Privacy />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Privacy/Privacy.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Privacy: React.FC = () => {
+  return (
+    <section className="w-full h-screen bg-white text-black font-medium flex flex-col m-auto items-center">
+      <h2>3D 손병호게임 (K-HANDGAME) 개인정보처리방침</h2>
+      <p>
+        3D 손병호게임 웹사이트와 어플리케이션은 사용자의 개인정보를 수집, 저장,
+        또는 활용하지 않습니다.
+        <br />
+        해당 소프트웨어는 사용자의 브라우징 활동에 관련된 정보를 수집하거나
+        개인정보를 요구하지 않습니다.
+      </p>
+      이 개인정보 처리 방침은 3D 손병호게임에만 적용되며, 본 소프트웨어 외의
+      외부 링크 또는 리디렉션에 대해서는 해당 링크의 개인정보 처리 방침이
+      적용됩니다.
+      <br />
+      <br />
+      개인정보 처리 방침에 대한 문의 사항이 있으시다면, 아래 이메일로 문의해
+      주십시오.
+      <br />
+      📧 mei.devdance@gmail.com
+      <br />
+      <br />
+      본 개인정보 처리 방침은 작성일로부터 효력이 발생하며, 변경사항이 있을 경우
+      본 페이지를 통해 공지됩니다.
+      <br />
+      2024.03.14
+    </section>
+  );
+};
+
+export default Privacy;
