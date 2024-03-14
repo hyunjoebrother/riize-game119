75ab38c9bbd8c921dc6ae1117f53654beced7576
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-03-14 13:17:27 +0900

    feat: implement i18n ENG version

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Trans } from "react-i18next";
+import { useLanguage } from "../../components/Translation/languageContext";
 import {
   shareKakao,
   shareTwitter,
@@ -9,23 +11,29 @@ import twitter from "../../assets/images/icon-twitter.png";
 import clip from "../../assets/images/icon-clip.png";
 
 const Footer: React.FC = () => {
+  const { language } = useLanguage();
+
   return (
     <div className="w-full h-full 2xs:my-8 xs:my-6 2sm:my-6 my-10 py-4 bg-black flex flex-row 2xs:gap-6 xs:gap-8 2sm:gap-10 gap-12 lg:gap-28 justify-center">
       <div className="flex flex-col gap-4 text-center text-white 2xs:text-[10px] text-xs font-light justify-start items-center">
         <h3>
-          <p className="2xs:text-xs text-sm font-bold">튜토리얼 하러가기</p>
+          <p className="2xs:text-xs text-sm font-bold">
+            <Trans i18nKey="translations:Tutorial">튜토리얼 하러가기</Trans>
+          </p>
           RIIZE GAME119
         </h3>
         <div>
           <a href="/">
             <button className="2xs:px-3 px-4 py-2 2xs:text-xs text-sm bg-orange-500 text-white font-bold rounded-md">
-              메인으로 이동하기
+              <Trans i18nKey="translations:GoToMain">메인으로 이동하기</Trans>
             </button>
           </a>
         </div>
       </div>
       <div className="flex flex-col gap-4 lg:gap-6 text-white 2xs:text-xs text-sm font-bold justify-start items-center">
-        <h3>친구한테 SNS 공유하기</h3>
+        <h3>
+          <Trans i18nKey="translations:Sharing">친구한테 SNS 공유하기</Trans>
+        </h3>
         <ul className="flex flex-row justify-end list-none gap-4 lg:gap-3">
           <li onClick={() => shareKakao()}>
             <img
--- src/components/QuizCard/QuizCard.tsx
+++ src/components/QuizCard/QuizCard.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Trans } from "react-i18next";
+import { useLanguage } from "../../components/Translation/languageContext";
 
 interface QuizCardProps {
   quizNum: number;
@@ -6,10 +8,22 @@ interface QuizCardProps {
 }
 
 const QuizCard: React.FC<QuizCardProps> = ({ quizNum, quizContent }) => {
+  const { language, changeLanguage } = useLanguage();
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem("language");
+    changeLanguage(savedLanguage);
+  });
+
   return (
     <section className="w-auto h-auto 2xs:min-w-48 2xs:max-w-56 xs:min-w-64 xs:max-w-72 2sm:min-w-72 2sm:max-w-80 sm:min-w-[300px] sm:max-w-96 tb:min-w-80 tb:max-w-[488px] lg:min-w-80 lg:max-w-[526px] 2xs:p-4 xs:p-4 p-5 flex flex-col justify-center items-start rounded-2xl bg-orange-500 text-white">
-      <p className="2xs:text-sm xs:text-base 2sm:text-base text-lg font-bold">질문 {quizNum}번</p>
-      <span className="2xs:mt-1 2sm:mt-1 xs:mt-1 mt-3 2xs:text-xs xs:text-sm 2sm:text-sm text-base font-light">{quizContent}</span>
+      <p className="2xs:text-sm xs:text-base 2sm:text-base text-lg font-bold">
+        <Trans i18nKey="translations:qTitle">질문</Trans>
+        {quizNum}
+        <Trans i18nKey="translations:qTitleCount">번</Trans>
+      </p>
+      <span className="2xs:mt-1 2sm:mt-1 xs:mt-1 mt-3 2xs:text-xs xs:text-sm 2sm:text-sm text-base font-light">
+        {quizContent}
+      </span>
     </section>
   );
 };
